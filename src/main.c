151069e7eb1afe39e81f189095c107a9e20d3f86
Thawan Camara <thawancamara@gmail.com>
2026-02-16 21:37:20 -0300

    wip

--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include "libft.h"
 
-int	main(void)
+enum {WHITE, GREEN, RED, BLUE, ORANGE, YELLOW};
+
+const char	*get_color_str(int id);
+void		init_cube(int **cube);
+
+
+int	main(int argc, char *argv[])
 {
-	printf("Hello %ld\n", ft_strlen("Hi"));
+	int	cube[6][9] = {0};
+
+	(void)argc;
+	(void)argv;
+	printf("%s\n", get_color_str(atoi(argv[1])));
 	return (0);
+}
+
+const char	*get_color_str(int id)
+{
+	const char	*color_str[] = {
+		"White",
+		"Green",
+		"Red",
+		"Orange",
+		"Blue",
+		"Yellow"
+	};
+
+	return (color_str[id % 6]);
+}
+
+void		init_cube(int **cube)
+{
 }
